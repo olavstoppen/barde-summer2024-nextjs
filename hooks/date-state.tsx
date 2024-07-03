3e26d756985ab6a5a8290efb8adce5b3e42f6f5a
Hannareie <hannahreiestad@gmail.com>
2024-07-03 10:24:46 +0200

    resolve conflicts

--- hooks/date-state.tsx
+++ hooks/date-state.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import React from 'react';
 import { useState } from 'react';
 
 export const useSearchState = () => {
-    const [dateState, setDateState] = React.useState<Dayjs | null>(
-        dayjs('2024-04-17'),
-    );
+    const [dateState, setDateState] = React.useState<Dayjs | null>(dayjs('2024-04-17'));
 
     return { dateState, setDateState };
 };

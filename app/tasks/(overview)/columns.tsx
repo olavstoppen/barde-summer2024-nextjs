71dee64113b72744e245b210b276cfe172b9b927
Hannareie <hannahreiestad@gmail.com>
2024-07-08 08:43:24 +0200

    Fixed small issues

--- app/tasks/(overview)/columns.tsx
+++ app/tasks/(overview)/columns.tsx
@@ -1,97 +1,85 @@
-'use client'
+'use client';
 
-import { ColumnDef } from '@tanstack/react-table'
+import { ColumnDef } from '@tanstack/react-table';
 
-import { ArrowUpDown } from 'lucide-react'
+import { ArrowRightIcon } from '@heroicons/react/24/solid';
 
-import { ArrowRightIcon } from '@heroicons/react/24/solid'
-
-import Link from 'next/link'
-import { Button } from '@/app/ui/tasks/table/button'
-import { task } from '@/app/lib/types'
+import Link from 'next/link';
+import { Button } from '@/app/ui/tasks/table/button';
+import { task } from '@/app/lib/types';
+import { ArrowUpIcon, ChevronUpDownIcon } from '@heroicons/react/24/outline';
 
 export const columns: ColumnDef<task>[] = [
-  {
-    accessorKey: 'id',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant='default'
-          className='text-black rounded-xl'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          ID
-          <ArrowUpDown className='ml-2 h-4 w-4' />
-        </Button>
-      )
-    }
-  },
-  {
-    accessorKey: 'name',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant='default'
-          className='text-black'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Oppgave
-          <ArrowUpDown className='ml-2 h-4 w-4' />
-        </Button>
-      )
-    }
-  },
-  {
-    accessorKey: 'start_date',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant='default'
-          className='text-black'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Start date
-          <ArrowUpDown className='ml-2 h-4 w-4' />
-        </Button>
-      )
+    {
+        accessorKey: 'id',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="default"
+                    className="text-black rounded-xl"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    ID
+                    <ChevronUpDownIcon className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
     },
-  },
-  {
-    accessorKey: 'machine',
-    header: ({ column }) => {
-      return (
-        <div
-          className='flex text-black'
-        >
-          Maskin
-        </div>
-      )
+    {
+        accessorKey: 'name',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="default"
+                    className="text-black"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    Oppgave
+                    <ChevronUpDownIcon className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
     },
-  },
-  {
-    accessorKey: 'type',
-    header: ({ column }) => {
-      return (
-        <div
-          className='flex text-black'
-        >
-          Type
-        </div>
-      )
+    {
+        accessorKey: 'start_date',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="default"
+                    className="text-black"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+                >
+                    Start date
+                    <ChevronUpDownIcon className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
     },
-  },
-  {
-    id: 'actions',
-    header: "",
-    cell: ({ row }) => {
-      return(
-      <Link
-      href={`/tasks/${(parseInt(row.id)+1).toString()}`}
-      className="flex h-10 rounded-xl items-center rounded-lg px-4 text-sm font-medium"
-  >
-      <ArrowRightIcon className="h-5 md:ml-4 text-pri rounded-xl" />
-      <span className="hidden md:block ml-3 text-pri rounded-xl">Se info</span>
-  </Link>)
-    }
-  }
-]
+    {
+        accessorKey: 'machine',
+        header: () => {
+            return <div className="flex text-black">Maskin</div>;
+        },
+    },
+    {
+        accessorKey: 'type',
+        header: () => {
+            return <div className="flex text-black">Type</div>;
+        },
+    },
+    {
+        id: 'actions',
+        header: '',
+        cell: ({ row }) => {
+            return (
+                <Link
+                    href={`/tasks/${(parseInt(row.id) + 1).toString()}`}
+                    className="flex h-10 rounded-xl items-center rounded-lg px-4 text-sm font-medium"
+                >
+                    <ArrowRightIcon className="h-5 md:ml-4 text-pri rounded-xl" />
+                    <span className="hidden md:block ml-3 text-pri rounded-xl">Se info</span>
+                </Link>
+            );
+        },
+    },
+];
--- app/tasks/(overview)/page.tsx
+++ app/tasks/(overview)/page.tsx
@@ -1,26 +1,24 @@
-
 import { task } from '@/app/lib/types';
-import { columns } from './columns'
+import { columns } from './columns';
 import { promises as fs } from 'fs';
 import { DataTable } from '@/app/ui/tasks/data-table';
 
 async function getTasks(): Promise<task[]> {
+    const file = await fs.readFile(process.cwd() + '/app/lib/data.json', 'utf8');
+    const data = JSON.parse(file);
 
-  const file = await fs.readFile(process.cwd() + '/app/lib/data.json', 'utf8');
-  const data = JSON.parse(file);
-
-  return data.task;
+    return data.task;
 }
 
 export default async function Page() {
-  const data = await getTasks()
+    const data = await getTasks();
 
-  return (
-    <section>
-      <div className='container'>
-        <h1 className='text-3xl'>Tasks</h1>
-        <DataTable columns={columns} data={data} />
-      </div>
-    </section>
-  )
+    return (
+        <section>
+            <div className="max-w-7xl pt-4">
+                <h1 className="text-3xl max-w-xl pb-6">Her er alle dine tidligere vedlikeholdsoppgaver</h1>
+                <DataTable columns={columns} data={data} />
+            </div>
+        </section>
+    );
 }
